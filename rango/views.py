75f68d76cc69ed6966509013d7c383829b39e024
David132639 <David132639@users.noreply.github.com>
2019-01-28 16:40:26 +0000

    Up to 6.3 exclusive

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     #return HttpResponse("Rango says hey there partner!<br><a href=\"\\rango\\about\">About page</a>")
-    context_dicti = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #context_dicti = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dicti = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dicti)
 
 def about(request):
